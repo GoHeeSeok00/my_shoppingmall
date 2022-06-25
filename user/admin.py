e5f3f4df79c07323bfbc88da93b09937199031b8
Github <weeds1590@gmail.com>
2022-06-25 22:48:25 +0900

    Update: user/admin.py CustomUserAdmin 클래스 내 profile_image 필드 추가
    list_display, fieldsets에 profile_image 필드를 추가해서 admin 페이지에서 볼 수 있게 수정

--- user/admin.py
+++ user/admin.py
@@ -10,14 +10,14 @@ class CustomUserAdmin(BaseUserAdmin):
     model = UserModel
     ordering = ('userid',)
 
-    list_display = ("id", "userid", "name")
+    list_display = ("id", "userid", "name", "profile_image")
     list_display_links = ("id", "userid")
     list_filter = ("userid", "name")
     search_fields = ("userid", "name")
     readonly_fields = ("userid", "join_date")
 
     fieldsets = (
-        ("info", {"fields": ("userid", "password", "email", "name", "gender", "date_of_birth", "mobile_number",
+        ("info", {"fields": ("userid", "password", "profile_image", "email", "name", "gender", "date_of_birth", "mobile_number",
                              "introduce", "join_date", "is_secession")}),
         ("Agreement", {"fields": ("is_terms_of_service", "is_privacy_policy", "is_receive_marketing_info")}),
         ("permissions", {"fields": ("is_admin", "is_seller", "is_active")})
