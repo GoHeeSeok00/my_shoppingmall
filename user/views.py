44fb25d62de7057b0f7be0c61e84bc6db15e870f
Github <weeds1590@gmail.com>
2022-06-25 22:58:52 +0900

    Add: user/views.py UserApiView 클래스 추가
    get, post 메소드를 가지고 있는 UserApiView 클래스를 추가했습니다.
    아직 serializers.py를 작성하지 않아 기본 구조만 가지고 있습니다.

--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+""""""
 # Create your views here.
+class UserApiView(APIView):
+
+    def get(self):
+        return Response()
+
+    def post(self):
+        return Response()
