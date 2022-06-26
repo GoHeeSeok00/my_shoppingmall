697992704824f61456af2790dc8d278a66302bcd
Github <weeds1590@gmail.com>
2022-06-26 12:49:56 +0900

    Add: user/views.py custom permission 추가(IsAdminOrCreateOnly)
    모든 사용자는 post 메소드일때만 접근이 가능하고 관리자는 모두 가능한 권한을 위해 custom permissino 추가

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -7,9 +8,27 @@ from user.models import User as UserModel
 from user.serializers import UserSerializer
 
 """"""
+# create custom permission
+class IsAdminOrCreateOnly(permissions.BasePermission):
+    """
+    모든 사용자는 쓰기만 가능
+    관리자는 모두 가능
+    """
+    SAFE_METHODS = ('POST', )
+    message = '접근 권한이 없습니다.'
+
+    def has_permission(self, request, view):
+        user = request.user
+
+        if request.method in self.SAFE_METHODS or user.is_admin:
+            return True
+
+        return False
+
+
 # Create your views here.
 class UserApiView(APIView):
-
+    permission_classes = [IsAdminOrCreateOnly]
     def get(self):
         user_serializer = UserSerializer(UserModel.objects.all(), many=True).data
         return Response(user_serializer, status=status.HTTP_200_OK)
