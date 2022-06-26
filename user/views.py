fb7febeffa5522084203956b831addcb9063dc8e
Github <weeds1590@gmail.com>
2022-06-26 12:34:24 +0900

    Add: user/views.py UserApiView get 메소드 추가
    전체 유저를 조회하기 위해 UserSerializer를 사용한 get 메소드 생성

--- user/views.py
+++ user/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from user.models import User as UserModel
+from user.serializers import UserSerializer
 
 """"""
 # Create your views here.
 class UserApiView(APIView):
 
     def get(self):
-        return Response()
+        user_serializer = UserSerializer(UserModel.objects.all(), many=True).data
+        return Response(user_serializer, status=status.HTTP_200_OK)
 
     def post(self):
         return Response()
