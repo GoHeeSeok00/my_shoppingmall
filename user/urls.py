f6cf106a528b76b0826c21392a2295d79a3dfe7e
Github <weeds1590@gmail.com>
2022-06-26 15:12:46 +0900

    Add: user/urls.py 로그인, 로그아웃 api url에 연결

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from user.views import UserApiView
+from user.views import UserApiView, LoginApiView, LogoutApiView
 
 app_name = "user"
 
 urlpatterns = [
     path('', UserApiView.as_view(), name='user'),  # class엔 as_view()를 붙여주어야 한다.
+    path('login/', LoginApiView.as_view(), name='login'),
+    path('logout/', LogoutApiView.as_view(), name='logout'),
 ]
