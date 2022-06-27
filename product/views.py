472a94350b67887e015b3571fb155932db6fbb84
Github <weeds1590@gmail.com>
2022-06-28 00:11:57 +0900

    Add: product/views.py ProductApiView 생성
    전체 상품 목록을 조회할 수 있고 등록할 수 있는 api 생성

--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+""""""
 # Create your views here.
+class ProductApiView(APIView):
+    # permission_classes = []
+
+    def get(self, request):
+        return Response()
+
+    def post(self, request):
+        return  Response()
