431f6d0ceb449174837ccc14f1d8af1ad9c857e0
Github <weeds1590@gmail.com>
2022-06-28 01:18:55 +0900

    Add: product/admin.py product 관리자 페이지 생성

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from product.models import Product as ProductModel
+from product.models import ProductImage as ProductImageModel
+from product.models import ProductOption as ProductOptionModel
+from product.models import Category as CategoryModel
+
 # Register your models here.
+admin.site.register(ProductModel)
+admin.site.register(ProductImageModel)
+admin.site.register(ProductOptionModel)
+admin.site.register(CategoryModel)
