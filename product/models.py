e062d770172ef9b467d807bdde3368d223dd4ef8
Github <weeds1590@gmail.com>
2022-06-27 22:13:43 +0900

    Add: product/models.py ProductOption 테이블 추가
    사이즈와 컬러같은 상품의 옵션을 위한 테이블 생성
    옵션별로 가격과 할인 가격을 책정할 수 있게 모델 설계

--- product/models.py
+++ product/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from user.models import User as UserModel
@@ -32,3 +33,18 @@ class ProductImage(models.Model):
     image3 = models.ImageField("이미지3", upload_to="product/image/",  max_length=None, null=True, blank=True)
     image4 = models.ImageField("이미지4", upload_to="product/image/",  max_length=None, null=True, blank=True)
     image5 = models.ImageField("이미지5", upload_to="product/image/",  max_length=None, null=True, blank=True)
+
+
+class ProductOption(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    product = models.ForeignKey(to=Product, verbose_name="상품", on_delete=models.CASCADE)
+    name = models.CharField("상품명", max_length=100)
+    price = models.IntegerField("가격", validators=[MinValueValidator(0)])
+    quantity = models.IntegerField("수량", validators=[MinValueValidator(0)])
+    is_discount = models.BooleanField("할인 여부", default=False)
+    discount_price = models.IntegerField("할인 가격", validators=[MinValueValidator(0)], null=True, blank=True)
+    discount_start_date = models.DateTimeField("할인 시작일", null=True, blank=True)
+    discount_end_date = models.DateTimeField("할인 종료일", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
