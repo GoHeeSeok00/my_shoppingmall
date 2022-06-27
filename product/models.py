f31c7cf9b6cfbf2f4531cd00bd4ef66b8bc11d8f
Github <weeds1590@gmail.com>
2022-06-27 21:33:18 +0900

    Add: product/models.py Category, Product 모델 추가
    Category 테이블과 Product 테이블 M2M 관계로 연결

--- product/models.py
+++ product/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from user.models import User as UserModel
+
+""""""
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField("카테고리명", max_length=20)
+
+class Product(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(to=UserModel, verbose_name="판매자", on_delete=models.CASCADE)
+    title = models.CharField("상품 제목", max_length=100)
+    category = models.ManyToManyField(to=Category, verbose_name="카테고리")
+    thumbnail = models.ImageField("썸네일", upload_to="product/thumbnail/", max_length=None)
+    description = models.TextField("상품 설명")
+    view_count = models.BigIntegerField("상품 조회수", default=0)
+    is_active = models.BooleanField("활성화 상태", default=False)
+    created_at = models.DateTimeField("작성시간", auto_now_add=True)
+    updated_at = models.DateTimeField("수정시간", auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
