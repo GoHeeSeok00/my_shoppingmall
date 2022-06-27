1c1bdf7f2b762eef675259a5500835933c744f2a
Github <weeds1590@gmail.com>
2022-06-27 18:30:50 +0900

    Add: user/urls.pt UserAddressApiView 주소 추가
    회원 주소를 조회, 등록, 수정, 삭제하는 api 연결하는 주소 추가

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from user.views import UserApiView, LoginApiView, LogoutApiView, UserDetailApiView
+from user.views import UserApiView, LoginApiView, LogoutApiView, UserDetailApiView, UserAddressApiView
 
 app_name = "user"
 
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', UserApiView.as_view(), name='user'),  # class엔 as_view()를 붙여주어야 한다.
     path('login/', LoginApiView.as_view(), name='login'),
     path('logout/', LogoutApiView.as_view(), name='logout'),
+    path('address/', UserAddressApiView.as_view(), name='user_address'),
+    path('address/<obj_id>/', UserAddressApiView.as_view(), name='user_address'),
     path('<obj_id>/', UserDetailApiView.as_view(), name='user_detail'),
 ]
